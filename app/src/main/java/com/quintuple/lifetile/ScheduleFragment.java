7150f5969c9778735183260e4366b14a23d83a19
Marcus <1605923j@student.tp.edu.sg>
2018-08-25 15:34:12 +0800

    Schedule

--- app/src/main/java/com/quintuple/lifetile/ScheduleFragment.java
+++ app/src/main/java/com/quintuple/lifetile/ScheduleFragment.java
@@ -1,11 +1,19 @@
 package com.quintuple.lifetile;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class ScheduleFragment extends Fragment {
     @Override
@@ -14,6 +22,25 @@ public class ScheduleFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.schedule_fragment, container, false);
         FloatingActionButton floatingActionButton = view.findViewById(R.id.floating);
+        int year, month, day, DIALOG_ID = 0;
+
+        // DatePicker
+        final Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, day);
+            }
+        };
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         return view;
     }
 }
